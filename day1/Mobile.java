79c0d4d71dc0051f89e76d0932bcfc6cdf41485e
rajulnair <106631620+rajulnair@users.noreply.github.com>
2023-03-30 16:42:04 +1300

    Add files via upload

---
+++ day1/Mobile.java
@@ -0,0 +1,25 @@
+package week1.day1;
+
+public class Mobile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	Mobile phone = new Mobile();
+	
+	phone.makeCall();
+	phone.sendSMS();
+	}
+	
+	public void sendSMS() {
+		System.out.println("From Oppo.");
+	}
+	
+	public void makeCall() {
+		System.out.println("Calling my friend.");
+
+	}
+	
+	
+
+}
