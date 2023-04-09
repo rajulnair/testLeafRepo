175edd17adda1cf85c01e8a5857ffd567d9057e5
rajulnair <106631620+rajulnair@users.noreply.github.com>
2023-04-09 15:29:10 +1200

    Add files via upload

---
+++ week1/day1/Iphone.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class Iphone {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public void makeCall() {
+		System.out.println("make a call, Iphone");
+
+	}
+	
+	public void model() {
+		System.out.println("what's your model, Iphone?");
+
+	}
+
+}
---
+++ week1/day1/Smartphone.java
@@ -0,0 +1,11 @@
+package week1.day1;
+
+public class Smartphone {
+
+	public static void main(String[] args) {
+		Iphone cell = new Iphone();
+		cell.makeCall();
+		cell.model();
+	}
+
+}
